d03fd7a7fd8c21a5c724ea27b5cfc0237627e6ad
marcinoles87 <marcinoles87@gmail.com>
2023-06-14 18:47:02 +0200

    add function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
+import { useState } from 'react';
 import './App.css';
 import NotesList from './components/NotesList';
 
 function App() {
+
+const [note , setNote] = useState('')
+
+const handleOnChange = (e) => {
+  e.preventDefault()
+  console.log(e.target.value)
+}
+
+
   return (
     <div className="App">
-      <input placeholder='add your note'></input>
-      <NotesList></NotesList>
+      <input placeholder='add your note' onChange={handleOnChange}></input>
+      <NotesList note={note} setNote={setNote}></NotesList>
     </div>
   );
 }
